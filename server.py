d0cacb7cb407059ae22ff8a8f5cea6cd54cbba1b
chenyy <1280129660@qq.com>
2018-12-07 22:25:45 +0800

    3-2

--- server.py
+++ server.py
@@ -1,37 +1,40 @@
 import socket
+from utils import log
+from routes import *
+import urllib.parse
 
 
-def log(*args, **kwargs):
-    print("log: ", *args, **kwargs)
+class Request:
+    def __init__(self):
+        self.path = '/'
+        self.method = 'GET'
+        self.query = {}
+        self.body = ''
 
+    def form(self):
+        """
+        解析body，返回表单提价参数参数字典
+        :return:
+        """
+        # body = urllib.parse.unquote(self.body)
+        # log.log("self.body: ", body)
+        params = self.body.split('&')
+        args = {}
+        for param in params:
+            log.log('param: ', param)
+            k, v = param.split('=')
+            # 解码空格unquote_plus
+            v = urllib.parse.unquote_plus(v, encoding='utf-8')
+            args[k] = v
 
-def route_index():
-    headers = "HTTP/1.1 200 ok\r\nContent-Type: text/html\r\n"
-    body = '<h1>Hello World</h1><img src="doge.gif"/>'
-    r = headers + "\r\n" + body
-    return r.encode(encoding='utf-8')
+        return args
 
-
-def route_image():
-    with open('./templates/doge.gif', 'rb') as f:
-        return f.read()
-    # pass
-
-
-def error(status_code=404):
-    error_info = {
-        404: b'HTTP/1.1 404 Not Found\r\n\r\n<h1>Not Found</h1>'
-    }
-
-    return error_info.get(status_code, b'')
-
-def response_for_path(path):
-    url_router = {
-        '/' : route_index(),
-        '/doge.gif': route_image(),
-    }
-
-    return url_router.get(path, error())
+    def parse_request(self, request):
+        header = request.split("\r\n\r\n")[0]
+        self.body = request.split("\r\n\r\n")[1]
+        headers = header.split("\r\n")
+        self.method = headers[0].split(' ')[0]
+        self.path = headers[0].split(' ')[1]
 
 
 def run(host='127.0.0.1', port=2000):
@@ -53,23 +56,31 @@ def run(host='127.0.0.1', port=2000):
             buffer_size = 1000
             while True:
                 req = connection.recv(buffer_size)
+                if len(req.decode('utf-8').split()) < 2:
+                    continue
                 r += req
                 if len(r) < buffer_size:
                     break
 
-            log('ip and port: ', address)
-            log('request: ', r.decode('utf-8'))
+            log.log('ip and port: ', address)
+            log.log('request: ', r.decode('utf-8'))
 
             request = r.decode('utf-8')
+            req = Request()
+            req.parse_request(request)
+            path = req.path
+            if req.body:
+                args = req.form()
+                log.log("self.args: ", args)
+            log.log("self.path: ", path)
 
             try:
                 path = request.split(" ")[1]
-                log('path: ', path)
+                log.log('path: ', path)
                 response = response_for_path(path)
-
                 connection.sendall(response)
             except Exception as e:
-                log('error: ', e)
+                log.log('error: ', e)
 
             connection.close()
 
